56e7712b09f98fa0061419251c901b9aaf7a8941
Michael Boyle <michaelboyle@smarttech.com>
2012-04-20 20:15:48 -0600

    The original push omitted the copyright statements on each file.

--- AmbireTeacherTomcatApplication/src/org/eun/itec/ambire/teacher/AmazonWebServices.java
+++ AmbireTeacherTomcatApplication/src/org/eun/itec/ambire/teacher/AmazonWebServices.java
@@ -1,3 +1,32 @@
+/*   Copyright (C) 2012, SMART Technologies.
+     All rights reserved.
+  
+     Redistribution and use in source and binary forms, with or without modification, are permitted
+     provided that the following conditions are met:
+   
+      * Redistributions of source code must retain the above copyright notice, this list of
+        conditions and the following disclaimer.
+   
+      * Redistributions in binary form must reproduce the above copyright notice, this list of
+        conditions and the following disclaimer in the documentation and/or other materials
+        provided with the distribution.
+   
+      * Neither the name of SMART Technologies nor the names of its contributors may be used to
+         endorse or promote products derived from this software without specific prior written
+         permission.
+   
+     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+     IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+     FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+     CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+     SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+     THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+     OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+     POSSIBILITY OF SUCH DAMAGE.
+   
+     Author: Michael Boyle
+*/
 package org.eun.itec.ambire.teacher;
 
 import java.net.InetAddress;
@@ -14,9 +43,9 @@ public final class AmazonWebServices {
 	private static final long IDENTITY_BIT_MASK = 0xFFFFFFFFFFFFFL;
 
 	public static void init(ServletContext context) {
-		ACCOUNT_ID = Deployment.getParameter(context, "AmazonWebServices.ACCOUNT_ID", "8044-4856-6677");
-		ACCESS_KEY = Deployment.getParameter(context, "AmazonWebServices.ACCESS_KEY", "AKIAI3V4DAFKSPPMC7GA");
-		SECRET_KEY = Deployment.getParameter(context, "AmazonWebServices.SECRET_KEY", "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");
+		ACCOUNT_ID = Deployment.getParameter(context, "AmazonWebServices.ACCOUNT_ID", null);
+		ACCESS_KEY = Deployment.getParameter(context, "AmazonWebServices.ACCESS_KEY", null);
+		SECRET_KEY = Deployment.getParameter(context, "AmazonWebServices.SECRET_KEY", null);
 		REGION = Deployment.getParameter(context, "AmazonWebServices.REGION", "EU_Ireland");
 		ENDPOINT = Deployment.getParameter(context, "AmazonWebServices.ENDPOINT", "eu-west-1.amazonaws.com");
 		try {
